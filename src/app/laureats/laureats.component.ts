f8b4ff272ad9cefdff221b3b42c9b58f77184af3
Omar Messaoud <Messaoud@macX.local>
2018-11-05 17:55:16 +0100

    commit pagination

--- src/app/laureats/laureats.component.ts
+++ src/app/laureats/laureats.component.ts
@@ -11,38 +11,158 @@ import {Router} from '@angular/router';
 export class LaureatsComponent implements OnInit {
   pageLaureats: any;
   motCle: string = "";
-  size: number = 5;
+  size: number = 2;
   currentPage: number = 0;
   pages: Array<number>;
 
+  public filiere = "";
+  public promotion = "";
+  public secteur = "";
+  public genre = "";
+  public province = "";
+  public organisme = "";
+
   //pageLaureats: {content:[]};
 
-  constructor(public  http: HttpClient, public laureatservice: LaureatsServices,
-              public router: Router) { }
+  constructor(public  httpClient: HttpClient,
+              public laureatservice: LaureatsServices,
+              public router: Router) {
+
+
+
+
+  }
+
+
 
   ngOnInit() {
   }
 
+
+
+
+
+
   doSearch() {
+
+
+
+    let requeteFiltre = "http://192.168.43.233:9090/requestAny/select%20nom,prenom,photo,organisme.long,organisme.lat,filiere,promotion,organisme.secteur,genre,organisme.province,nomorganisme%20from%20utilisateur,organisme%20";
+    let clauseWhere = "";
+
+    console.log(this.secteur == "");
+
+    if(this.filiere != ""){
+
+      clauseWhere = clauseWhere + "filiere%20='" + this.filiere + "'%20and%20";
+
+
+    }
+
+    if(this.promotion != ""){
+
+      clauseWhere = clauseWhere + "promotion%20='" + this.promotion + "'%20and%20";
+
+    }
+
+    if(this.secteur != "" ){
+
+      clauseWhere = clauseWhere + "organisme.secteur%20='" + this.secteur + "'%20and%20";
+
+    }
+
+    if(this.genre != ""){
+
+      clauseWhere = clauseWhere + "genre%20='" + this.genre + "'%20and%20";
+
+    }
+
+
+    if(this.province != ""){
+
+      clauseWhere = clauseWhere + "organisme.province%20='" + this.secteur + "'%20and%20";
+
+    }
+
+    if(this.organisme != ""){
+
+      clauseWhere = clauseWhere + "nomorganisme%20='" + this.organisme + "'%20and%20";
+
+    }
+
+
+    if(clauseWhere != ""){
+
+      requeteFiltre = requeteFiltre + "where%20utilisateur.reforganisme=organisme.id%20and%20";
+
+      requeteFiltre = requeteFiltre +clauseWhere.substring(0,clauseWhere.length-9);
+
+    }
+    else{
+      requeteFiltre = requeteFiltre + "where%20utilisateur.reforganisme=organisme.id%20";
+
+    }
+
+
+
+    console.log(requeteFiltre);
+
+    this.laureatservice.nouveauFiltre(requeteFiltre,this.currentPage,this.size)
+      .subscribe( data => {
+
+        this.pageLaureats = (data as any).features;
+        console.log(this.pages);
+
+        console.log(requeteFiltre.replace("select%20nom,prenom,photo,organisme.long,organisme.lat,filiere,promotion,organisme.secteur,genre,organisme.province,nomorganisme","select%20count(*)%20"));
+
+        this.httpClient.get(requeteFiltre.replace("select%20nom,prenom,photo,organisme.long,organisme.lat,filiere,promotion,organisme.secteur,genre,organisme.province,nomorganisme","select%20count(*)%20"))
+          .subscribe(data2 => {
+
+
+            this.pages = new Array<number>(Number((data2 as any).features[0].count));
+
+          });
+
+
+
+      });
+
+
+
+
+    /*
     this.laureatservice.getLaureats(this.motCle, this.currentPage, this.size)
       .subscribe((data: any) => {
+
         this.pageLaureats = data;
         this.pages = new Array(data.totalPages);
         console.log(data.totalPages);
         console.log(data);
+
       }, err => {
+
         console.log(err);
+
       });
+
+      */
+
   }
+
 
   chercher() {
     this.doSearch();
   }
+
   goToPage(i: number) {
     this.currentPage = i;
     this.doSearch();
   }
+
   onEditLaureat(id: number) {
     this.router.navigate(['editLaureat',id]);
   }
+
+
+
 }
--- src/services/laureats.services.ts
+++ src/services/laureats.services.ts
@@ -1,26 +1,71 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Laureat} from '../Model/model.laureat';
+import {Subject} from 'rxjs';
+import {assertNumber} from '@angular/core/src/render3/assert';
+
+
 
 @Injectable()
 export class LaureatsServices {
-  constructor(public http: HttpClient) {
+
+  private laureatsList: any[] = [];
+  laureatsList$ = new Subject<any[]>();
+
+
+
+  constructor(public httpClient: HttpClient) {
+
+
+
 
   }
-  getLaureats(motCle: string, page: number, size: number ) {
-    return this.http.get('http://localhost:8080/chercherLaureats?mc='
-      + motCle + '&size=' + size + '&page=' + page);
+
+  //permet d'indiquer qu'une mise à jour au niveau du service est necessaire
+  emitList() {
+    this.laureatsList$.next(this.laureatsList);
+    console.log(this.laureatsList);
   }
+
+
+
+  public nouveauFiltre(requete : string, page : number, size : number){
+
+
+
+
+    requete = requete + "%20order%20by%20utilisateur.id%20limit%20" + size + "%20offset%20" + Number(size*page + size);
+
+    console.log(requete);
+
+    return this.httpClient.get(requete);
+
+
+
+
+
+  }
+
+
+
+  getLaureats(motCle: string, page: number, size: number ) {
+
+
+    return this.httpClient.get('http://localhost:8080/chercherLaureats?mc=' + motCle + '&size=' + size + '&page=' + page);
+
+
+  }
+
 
   getLaureat(id: number) {
-    return this.http.get('http://localhost:8080/laureats/' + id);
+    return this.httpClient.get('http://localhost:8080/laureats/' + id);
   }
 
   saveLaureats(laureat: Laureat ) {
-    return this.http.post('http://localhost:8080/laureats', laureat);
+    return this.httpClient.post('http://localhost:8080/laureats', laureat);
   }
   updateLaureat(laureat: Laureat ) {
-    return this.http.put('http://localhost:8080/laureats/' + laureat.id, laureat);
+    return this.httpClient.put('http://localhost:8080/laureats/' + laureat.id, laureat);
   }
 
 }
