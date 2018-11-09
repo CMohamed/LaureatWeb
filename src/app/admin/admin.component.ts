6a1c29a65d7a11e404ff513e4b4d033d6364f88b
CMohamed <chadad.mohamed@gmail.com>
2018-11-09 09:13:47 +0100

    Edit and admin +

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Subscription} from 'rxjs';
 import {LaureatsServices} from '../../services/laureats.services';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -11,11 +12,22 @@ export class AdminComponent implements OnInit {
 
   public laureatsListSubscription : Subscription;
   public laureatsList : any[];
+  public router :Router;
+
+  currentPage: number = 0;
+  filiere :string;
+  promotion :string;
+  secteur :string;
+  genre :string;
+  province :string;
+  organisme :string;
+  quota: number;
 
   constructor(public laureatsServices : LaureatsServices) {
     this.laureatsListSubscription = this.laureatsServices.laureatsList$.subscribe(
       (laureatsImported: any[]) => {
 
+        this.laureatsList = laureatsImported;
         console.log(laureatsImported);
 
       }
@@ -25,4 +37,22 @@ export class AdminComponent implements OnInit {
   ngOnInit() {
   }
 
+  doSearch() {
+
+    this.laureatsServices.nouveauFiltre(this.genre,this.filiere,this.province,this.organisme,this.secteur,this.promotion,this.quota);
+
+
+  }
+  chercher() {
+    this.doSearch();
+  }
+
+  goToPage(i: number) {
+    this.currentPage = i;
+    this.doSearch();
+  }
+
+  onEditLaureat(id: number) {
+    this.router.navigate(['editLaureat',id]);
+  }
 }
